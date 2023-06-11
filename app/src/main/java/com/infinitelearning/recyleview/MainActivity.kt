886970f9eafda5bf1e299e6a2f382d1d3579104c
MARS <sdamar321@gmail.com>
2023-06-11 20:45:43 +0700

    Initial commit

---
+++ app/src/main/java/com/infinitelearning/recyleview/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.infinitelearning.recyleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var foodList : ArrayList<Food>
+    private lateinit var foodAdapter: FoodAdapter
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerview)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        foodList = ArrayList()
+
+        foodList.add(Food(R.drawable.paneer_butter_masala, "Paneer Butter Masala"))
+        foodList.add(Food(R.drawable.pizza, "Pizza"))
+        foodList.add(Food(R.drawable.dosa, "Dosa"))
+        foodList.add(Food(R.drawable.veg_biryani, "Veg Biryani"))
+
+        foodList.add(Food(R.drawable.paneer_butter_masala, "Paneer Butter Masala"))
+        foodList.add(Food(R.drawable.pizza, "Pizza"))
+        foodList.add(Food(R.drawable.dosa, "Dosa"))
+        foodList.add(Food(R.drawable.veg_biryani, "Veg Biryani"))
+        
+        foodList.add(Food(R.drawable.paneer_butter_masala, "Paneer Butter Masala"))
+        foodList.add(Food(R.drawable.pizza, "Pizza"))
+        foodList.add(Food(R.drawable.dosa, "Dosa"))
+        foodList.add(Food(R.drawable.veg_biryani, "Veg Biryani"))
+
+        foodAdapter = FoodAdapter(foodList)
+        recyclerView.adapter = foodAdapter
+    }
+}
